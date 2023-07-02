adad6240c172f4e0110fd6dacf9a153a61a29fa0
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-07-02 09:48:38 +0300

    done

--- main.js
+++ main.js
@@ -4,6 +4,46 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.getElementById('modal');
+const errorMessage = document.getElementById('modal-message');
+const hearts = document.querySelectorAll('.like-glyph');
+
+// Hide the error modal initially
+errorModal.classList.add('hidden');
+
+// Function to handle the server response
+function handleServerResponse(response) {
+  if (response === 'success') {
+    // Change the heart to full and add activated class
+    event.target.classList.add('activated-heart');
+    event.target.classList.remove('like-glyph');
+  } else {
+    // Display error modal and message
+    errorMessage.textContent = 'Server Error';
+    errorModal.classList.remove('hidden');
+    // Hide the error modal after 3 seconds
+    setTimeout(() => {
+      errorModal.classList.add('hidden');
+    }, 3000);
+  }
+}
+
+// Handle click event on hearts
+for (let heart of hearts) {
+  heart.addEventListener('click', function () {
+    mimicServerCall()
+      .then(handleServerResponse)
+      .catch(() => {
+        // Display error modal and message
+        errorMessage.textContent = 'Network Error';
+        errorModal.classList.remove('hidden');
+        // Hide the error modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  });
+}
 
 
 
